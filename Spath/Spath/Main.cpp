dd46367e40e5b419db78f7ae12dbce6a0a9bca9f
Sebastian Sarnecki <ssarnecki34@gmail.com>
2018-04-02 14:08:16 +0200

    ..

--- Spath/Spath/Main.cpp
+++ Spath/Spath/Main.cpp
@@ -6,6 +6,7 @@
 #include<map>
 #include<list>
 #include<vector>
+#include <time.h>       /* time */
 #define MAXVALUE 2147483647
 using namespace std;
 
@@ -16,11 +17,10 @@ template <class T> void swapElements(T& a, T& b)
 
 struct Point
 {
-	int pathTo;
+	int Distance;
 	int Index; //index miasta
-//	bool visited;
 };
-struct Path 
+struct Path
 {
 	int startCity;
 	int targetCity;
@@ -35,22 +35,71 @@ struct City {
 	int NumberOfAdjacet;
 	int Index; //index miasta
 };
+
+//mapowanie indexow https://pastebin.com/kkZn123m
 //kolejka priorytetowa opara o pseudokod z książki Wprowadzenie do algorytmów Cormena. Kod pochodzi także z mojego projektu na AISD
 //link https://github.com/sarnecky/Algorytmy-i-struktury-danych/blob/master/SpacerPoGorach/SpacerPoGorach/Source.cpp
-struct Queue
+struct PriorityQueue
 {
-	Point *tab[9999999];
+	vector<Point> tab;
+	map<int, int> mapper; //index miasta, index w kolejce
 	int _numberOfElements;
+	void init(int n, int rootIndexCity, int rootDistance)
+	{
+		mapper.clear();
+		tab.clear();
+		_numberOfElements = n;
+
+		Point p;
+		tab.push_back(p); //root
+		p.Index = rootIndexCity;
+		p.Distance = rootDistance;
+		tab.push_back(p);
+		mapper.insert(pair<int, int>(p.Index, 1)); //startowe miasto zawsze na 1 miejscu w kolejce
+		int i = 0;
+		for (int i = 0; i < _numberOfElements; i++)
+		{
+			if (i == rootIndexCity)
+				continue;
+			Point p;
+			p.Index = i;
+			p.Distance = MAXVALUE;
+			tab.push_back(p);
+		}
+
+		int idCity = 0;
+		int idQueue = 1;
+		for (int i = 0; i < _numberOfElements; i++)
+		{
+			if (i == rootIndexCity)
+				continue;
+			else
+				idQueue++;
+
+			mapper.insert(pair<int, int>(i, idQueue));
+			idCity++;
+		}
+	}
+
+	void decreaseKey(int indexCity, int newDistance)
+	{
+		int indexInQueue = mapper.at(indexCity);
+		if (tab[indexInQueue].Distance < newDistance)
+			return;
+
+		tab[indexInQueue].Distance = newDistance;
+		heapyfiUp(indexInQueue);
+	}
 
 	void heapyfiUp(int i)
-	{ //przyspieszyc
-		int P = i / 2;
+	{
+		int P = (i - 1) / 2;
 
-		while (i > 1)
+		while (i > 0)
 		{
-			if (tab[P]->pathTo > tab[i]->pathTo)
+			if (tab[P].Distance > tab[i].Distance)
 			{
-				swapElements(tab[P], tab[i]);
+				swapCity(tab[P], tab[i]);
 			}
 			i = P;
 			P = i / 2;
@@ -63,21 +112,21 @@ struct Queue
 		int P = 2 * i + 1;
 		int min;
 
-		if (L <= _numberOfElements && tab[L]->pathTo < tab[i]->pathTo) // jezeli lewy element jest mniejszy od rodzica to trzeba go zamienic
+		if (L <= _numberOfElements && tab[L].Distance  < tab[i].Distance) // jezeli lewy element jest mniejszy od rodzica to trzeba go zamienic
 		{
 			min = L; //mniejszy stoi na lewej stronie
 		}
 		else min = i;
 
 
-		if (P <= _numberOfElements && tab[P]->pathTo < tab[min]->pathTo) // jezeli prawy element jest mniejszy od rodzica to trzeba go zamienic
+		if (P <= _numberOfElements && tab[P].Distance  < tab[min].Distance) // jezeli prawy element jest mniejszy od rodzica to trzeba go zamienic
 		{
 			min = P; //mniejszy stoi na prawej stronie
 		}
 
 		if (min != i)  //zamiana elementow i 
 		{
-			swapElements(tab[i], tab[min]);
+			swapCity(tab[i], tab[min]);
 			heapyfy(min);
 		}
 	}
@@ -90,45 +139,40 @@ struct Queue
 		}
 	}
 
-	void insertHeap(int index, int pathTo)
+	void insertHeap(int indexOfCity, int pathTo) //trza dodac index
 	{
 		_numberOfElements++;
-		tab[_numberOfElements] = new Point;
-		tab[_numberOfElements]->Index = index;
-		tab[_numberOfElements]->pathTo = pathTo;
-		//tab[_numberOfElements]->visited = false;
+		Point p;
+		p.Index = indexOfCity;
+		p.Distance = pathTo;
+		tab.push_back(p);
 		buildHeap();
+	}
+
+	void swapCity(Point& a, Point& b)
+	{
+		int temp = mapper.at(a.Index);
+		mapper.at(a.Index) = mapper[b.Index];
+		mapper.at(b.Index) = temp; //nowy index sie tworzy :( znalezc sposob
+		Point c(a); a = b; b = c;
 	}
 
 	Point removeHeap()
 	{
 		Point root;
-		root.pathTo = tab[1]->pathTo;
-		root.Index = tab[1]->Index;
-		//tab[1]->visited = true;
-		swapElements(tab[1], tab[_numberOfElements]);
+		root.Distance = tab[1].Distance;
+		root.Index = tab[1].Index;
+		swapCity(tab[1], tab[_numberOfElements]);
 		tab[_numberOfElements] = tab[0];
-		//tab[1] = tab[_numberOfElements];
 		_numberOfElements--;
 		buildHeap();
 		return root;
 	}
-	void wypisz()
-	{
-		int p;
-		Point liczba;
-		for (int i = 1; i <= _numberOfElements;)
-		{
-			p = tab[i]->pathTo;
-			liczba = removeHeap();
-			printf("%d ", p);
-		}
-	}
-
 };
 
+
 //algorytm dijkstry na podstawie źródła https://www.cse.ust.hk/~dekai/271/notes/L10/L10.pdf (strona 16)
-void getShortestPath(Queue *Q, map<int, City> *cities, int startCity,  map<int,int> targets, vector<Path> *paths)
+int getShortestPath(PriorityQueue *Q, map<int, City> *cities, int startCity, int targetCity, map<int, int> targets, vector<Path> *paths)
 {
 	Point u;
 	int visitedCities = 0;
@@ -136,82 +180,60 @@ void getShortestPath(Queue *Q, map<int, City> *cities, int startCity,  map<int,i
 	{
 		u = Q->removeHeap();
 		City currentCity = cities->at(u.Index);
+		if (u.Index == targetCity)
+			return u.Distance;
+
 		if (currentCity.Distance > 200000)
-			break;
-
-		if (targets.find(u.Index)!=targets.end()) //odwiedzamy jakis z docelowych indexow
-		{
-			int indexInPaths = targets.at(u.Index);
-			paths->at(indexInPaths).distance = cities->at(u.Index).Distance;
-
-			if(!paths->at(indexInPaths).visited)
-				visitedCities++;
-
-			paths->at(indexInPaths).visited = true;
-			if (visitedCities == targets.size())
-				return;
-		}
+			continue;
 
 		for (int v = 0; v < cities->at(u.Index).NumberOfAdjacet; v++)
 		{
-			Point adjacetPoint= currentCity.Adjacets[v];
+			Point adjacetPoint = currentCity.Adjacets[v];
 			City adjacentCity = cities->at(adjacetPoint.Index);
 
-			int distanceToV = currentCity.Distance + adjacetPoint.pathTo;//matrixOfDistances[u.Index][adjacetIndex].pathTo;
+			int distanceToV = currentCity.Distance + adjacetPoint.Distance;//matrixOfDistances[u.Index][adjacetIndex].pathTo;
 
-			if (distanceToV<= 200000 && !adjacentCity.Visited && distanceToV < adjacentCity.Distance)
+			if (!adjacentCity.Visited && distanceToV < adjacentCity.Distance)
 			{
 				cities->at(adjacetPoint.Index).Distance = distanceToV;
-				/*if (adjacetPoint.Index == targetCity)
-					return distanceToV;*/
-				Q->insertHeap(adjacetPoint.Index, cities->at(adjacetPoint.Index).Distance);
+				Q->decreaseKey(adjacetPoint.Index, distanceToV);// adjacetPoint.Index, cities->at(adjacetPoint.Index).Distance);
 			}
 		}
 		cities->at(u.Index).Visited = true;
 	}
-	//return cities[targetCity].at(targetCity).Distance;
+	return cities->at(targetCity).Distance;
 }
 void makeMagic()
 {
-	Queue *Q = new Queue;
+	PriorityQueue *Q = new PriorityQueue;
 	Q->_numberOfElements = 0;
 	map<int, City> *cities = new map<int, City>;
-	//ifstream plik, out;
-	//plik.open("dane.txt");
 	int testsCount = 0;
 	int citiesCount = 0;
 	int numberOfNeighbours = 0;
 
 	scanf("%d", &testsCount);
-	//plik >> testsCount;
-
 	for (int test = 0; test < testsCount; test++)
 	{
 		scanf("%d", &citiesCount);
-		//plik >> citiesCount;
-
 		map<string, int> *mapOfCities = new map<string, int>();
 		char cityName[11];
 		for (int hostPosition = 0; hostPosition < citiesCount; hostPosition++)
 		{
 			scanf("%s", &cityName);
-			//plik >> cityName;
 			mapOfCities->insert(pair<string, int>(cityName, hostPosition));
 			scanf("%d", &numberOfNeighbours);
-			//plik >> numberOfNeighbours;
 			City city;
 			for (int neighbour = 0; neighbour < numberOfNeighbours; neighbour++)
 			{
 				int a, b;
 				scanf("%d %d", &a, &b);
-				//plik >> a >> b;
-				//distances[hostPosition][a-1].pathTo = b;
 				Point p;
-				p.pathTo = b;
+				p.Distance = b;
 				p.Index = a - 1;
 				city.Adjacets.push_back(p);
 			}
-			
+
 			city.Distance = MAXVALUE;
 			city.Index = hostPosition;
 			city.Visited = false;
@@ -223,71 +245,60 @@ void makeMagic()
 		char cityA[11], cityB[11]; string start, end;
 		vector<Path> paths; //drogi do przejscia
 		scanf("%d", &numberOfPaths);
-		//plik >> numberOfPaths;
-		for (int i = 0; i < numberOfPaths; i++)
-		{
-			scanf("%s %s", &cityA, &cityB);
-			//plik >> cityA >> cityB;
-			Path path;
-			path.startCity = mapOfCities->at(cityA);
-			path.targetCity = mapOfCities->at(cityB);
-			path.distance = 0;
-			path.visited = false;
-			paths.push_back(path); 
-		}
-		
 		int visitedPaths = 0;
-		for (int city = 0; city < citiesCount; city++) 
+		for (int city = 0; city < numberOfPaths; city++)
 		{
-			map<int, int>targets; //index w results, index miasta
-			for (int p = 0; p < paths.size(); p++) 
-			{
-				if (!paths[p].visited && paths[p].startCity == city)
-				{
-					targets.insert(pair<int, int>(paths[p].targetCity, p));
-					visitedPaths++;
-				}
-			}
+			map<int, int>targets;
+			scanf("%s %s", &cityA, &cityB);
+			if (mapOfCities->find(cityA) == mapOfCities->end())
+				continue;//odwiedzamy jakis z docelowych indexow
+			if (mapOfCities->find(cityB) == mapOfCities->end())
+				continue;//odwiedzamy jakis z docelowych indexow
 
-			Queue *QCopy = new Queue(*Q);
+			int startCityIndex = mapOfCities->at(cityA);
+			int targetCityIndex = mapOfCities->at(cityB);
+
+			PriorityQueue *QCopy = new PriorityQueue();
 			map<int, City> *citiesCopy = new map<int, City>(*cities);
-			citiesCopy->at(city).Distance = 0;
-			QCopy->insertHeap(city, 0); // pierwszy wierzcholek ma najnizszy priorytet
-			getShortestPath(QCopy, citiesCopy, city, targets, &paths);
+			citiesCopy->at(startCityIndex).Distance = 0;
 
-			//dodatkowe sprawdzenie drogi powrotnej
-			for (int p = 0; p < paths.size(); p++)
-			{
-				if (!paths[p].visited && paths[p].targetCity == city)
-				{
-					paths[p].visited = true;
-					int pathsIndex = targets.at(paths[p].startCity);
-					paths[p].distance = paths[pathsIndex].distance;
-					visitedPaths++;
-				}
-			}
-			if (paths.size() == visitedPaths)
-				break;
-			delete QCopy;
-			citiesCopy->clear();
+			QCopy->init(citiesCount, startCityIndex, 0); // pierwszy wierzcholek ma najnizszy priorytet
+			cout << getShortestPath(QCopy, citiesCopy, startCityIndex, targetCityIndex, targets, &paths) << endl;
+
 		}
-
-		//wypisanie wyników
-		for (int i = 0; i < numberOfPaths; i++)
-		{
-			cout << paths.at(i).distance << endl;
-		}
-
-		mapOfCities->clear();
-		cities->clear();
 	}
 	delete Q;
 }
 
 int main()
 {
+	//PriorityQueue *QCopy = new PriorityQueue();
+	//QCopy->init(14, 0, 5); // pierwszy wierzcholek ma najnizszy priorytet
+	//QCopy->decreaseKey(1, 10);
+	//QCopy->decreaseKey(2, 2);
+	//QCopy->decreaseKey(3, 6);
+	//QCopy->decreaseKey(4, 2);
+	//QCopy->decreaseKey(5, 3);
+	//QCopy->decreaseKey(6, 7);
+	//QCopy->decreaseKey(7, 7);
+	//QCopy->decreaseKey(8, 6);
+	//QCopy->decreaseKey(9, 5);
+	//QCopy->decreaseKey(10, 11);
+	//QCopy->decreaseKey(11, 9);
+	//QCopy->decreaseKey(12, 11);
+ //   Point wynik = QCopy->removeHeap();
+	//wynik = QCopy->removeHeap();
+	//wynik = QCopy->removeHeap();
+	//wynik = QCopy->removeHeap();
+	//wynik = QCopy->removeHeap();
+	//wynik = QCopy->removeHeap();
+	//wynik = QCopy->removeHeap();
+	//wynik = QCopy->removeHeap();
+	//wynik = QCopy->removeHeap();
+	//wynik = QCopy->removeHeap();
+	//wynik = QCopy->removeHeap();
+	//wynik = QCopy->removeHeap();
 	makeMagic();
-
 	//getchar();
 	//getchar();
 	return 0;
---
+++ Spath/Spath/spath.cpp
@@ -0,0 +1,177 @@
+﻿//#include <iostream>
+//#include <fstream>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string>
+//#include<map>
+//#include<list>
+//#include<vector>
+//#include <time.h>       /* time */
+//#define MAXVALUE 2147483647
+//using namespace std;
+//
+//template <class T> void swapElements(T& a, T& b)
+//{
+//	T c(a); a = b; b = c;
+//}
+//
+//struct Point
+//{
+//	int Distance;
+//	int Index; //index miasta
+//};
+//struct Path
+//{
+//	int startCity;
+//	int targetCity;
+//	int distance;
+//	bool visited;
+//};
+//struct City {
+//
+//	int Distance;
+//	bool Visited;
+//	vector<Point> Adjacets;
+//	int NumberOfAdjacet;
+//	int Index; //index miasta
+//};
+////mapowanie indexow https://pastebin.com/kkZn123m
+////kolejka priorytetowa opara o pseudokod z książki Wprowadzenie do algorytmów Cormena. Kod pochodzi także z mojego projektu na AISD
+////link https://github.com/sarnecky/Algorytmy-i-struktury-danych/blob/master/SpacerPoGorach/SpacerPoGorach/Source.cpp
+//struct PriorityQueue
+//{
+//	vector<Point> tab;
+//	map<int, int> mapper; //index miasta, index w kolejce
+//	int _numberOfElements;
+//
+//	void init(int n) 
+//	{
+//		_numberOfElements = n;
+//		for (int i = 0; i <= n; i++)
+//		{
+//			Point p;
+//			int indexCity = i - 1;
+//			p.Index = indexCity;
+//			if (i == 0)
+//			{
+//				tab.push_back(p);
+//				continue;
+//			}
+//			else if (i == 1) {
+//				p.Distance = 0;
+//			}
+//			else {
+//				p.Distance = MAXVALUE; 
+//			}
+//			mapper.insert(pair<int, int>(indexCity, i));
+//			tab.push_back(p);
+//		}
+//	}
+//
+//	void decreaseKey(int indexCity, int newDistance)
+//	{
+//		int indexInQueue = mapper.at(indexCity);
+//		if (tab[indexInQueue].Distance < newDistance)
+//			return;
+//
+//		tab[indexInQueue].Distance = newDistance;
+//		heapyfiUp(indexInQueue);
+//	}
+//
+//	void heapyfiUp(int i)
+//	{ 
+//		int P = (i+1) / 2;
+//
+//		while (i > 1)
+//		{
+//			if (tab[P].Distance > tab[i].Distance)
+//			{
+//				swapCity(tab[P], tab[i]);
+//			}
+//			i = P;
+//			P = i / 2;
+//		}
+//	}
+//
+//	void heapyfy(int i)
+//	{
+//		int L = 2 * i;
+//		int P = 2 * i + 1;
+//		int min;
+//
+//		if (L <= _numberOfElements && tab[L].Distance  < tab[i].Distance) // jezeli lewy element jest mniejszy od rodzica to trzeba go zamienic
+//		{
+//			min = L; //mniejszy stoi na lewej stronie
+//		}
+//		else min = i;
+//
+//
+//		if (P <= _numberOfElements && tab[P].Distance  < tab[min].Distance) // jezeli prawy element jest mniejszy od rodzica to trzeba go zamienic
+//		{
+//			min = P; //mniejszy stoi na prawej stronie
+//		}
+//
+//		if (min != i)  //zamiana elementow i 
+//		{
+//			swapCity(tab[i], tab[min]);
+//			heapyfy(min);
+//		}
+//	}
+//
+//	void buildHeap()
+//	{
+//		for (int i = _numberOfElements / 2; i >= 1; i = i / 2)
+//		{
+//			heapyfy(i);
+//		}
+//	}
+//
+//	void insertHeap(int indexOfCity, int pathTo) //trza dodac index
+//	{
+//		_numberOfElements++;
+//		Point p;
+//		p.Index = indexOfCity;
+//		p.Distance = pathTo;
+//		tab.push_back(p);
+//		buildHeap();
+//	}
+//
+//	void swapCity(Point& a, Point& b) 
+//	{
+//		int temp= mapper[a.Index];
+//		mapper[a.Index] = mapper[b.Index];
+//		mapper[b.Index] = temp;
+//		Point c(a); a = b; b = c;
+//	}
+//
+//	Point removeHeap()
+//	{
+//		Point root;
+//		root.Distance = tab[1].Distance;
+//		root.Index = tab[1].Index;
+//		swapCity(tab[1], tab[_numberOfElements]);
+//		tab[_numberOfElements] = tab[0];
+//		_numberOfElements--;
+//		buildHeap();
+//		return root;
+//	}
+//};
+//
+//
+//int main()
+//{
+//	PriorityQueue *p = new PriorityQueue();
+//	p->init(5);
+//	p->removeHeap();
+//	p->decreaseKey(1, 2);
+//	p->decreaseKey(2, 7);
+//	p->removeHeap();
+//	p->decreaseKey(2, 5);
+//	p->decreaseKey(3, 10);
+//	p->decreaseKey(4, 7);
+//	p->removeHeap();
+//	p->decreaseKey(3, 6);
+//	p->removeHeap();
+//	p->removeHeap();
+//	return 0;
+//}
